114eb5e0358c84e4eb80c08e914c3563cd62d3de
“hhhhhhzj” <2327475180@qq.com>
2023-03-02 15:29:48 +0800

    添加40

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,6 +4,7 @@ import Home from '../views/Home.vue'
 import Money from '@/views/Money.vue'
 import Labels from '@/views/Labels.vue'
 import Statistics from '@/views/Statistics.vue'
+import NotFound from '@/views/NotFound.vue'
 
 Vue.use(VueRouter)
 
@@ -12,18 +13,22 @@ const routes: Array<RouteConfig> = [
     path: '/',
     redirect: '/money'
   },
-{
-  path: '/money',
-  component: Money
-},
-{
-  path: '/labels',
-  component: Labels
-},
-{
-  path: '/statistics',
-  component: Statistics
-}
+  {
+    path: '/money',
+    component: Money
+  },
+  {
+    path: '/labels',
+    component: Labels
+  },
+  {
+    path: '/statistics',
+    component: Statistics
+  },
+  {
+    path: '*',
+    component: NotFound
+  }
 ]
 
 const router = new VueRouter({
---
+++ src/views/NotFound.vue
@@ -0,0 +1,25 @@
+<!--
+ * @Author: “hhhhhhzj” “2327475180@qq.com”
+ * @Date: 2023-03-02 15:21:13
+ * @LastEditors: “hhhhhhzj” “2327475180@qq.com”
+ * @LastEditTime: 2023-03-02 15:21:23
+ * @FilePath: \morny\src\views\NotFound.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
+<template>
+  <div>
+    <div>你当前页面不存在，请检查网址是否正确</div>
+    <div><router-link to="/">返回首页</router-link></div>
+  </div>
+  
+</template>
+
+<script lang="ts">
+  export default {
+    name: "NotFound"
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
